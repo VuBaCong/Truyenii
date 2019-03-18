da2bad63865f0fc4e0a81860aa8375a861903e2c
VuBaCong <congvb62@wru.vn>
2019-03-18 21:48:10 +0700

    ver1

---
+++ app/src/main/java/com/example/truyenii/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.truyenii;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
